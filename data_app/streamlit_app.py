2501db6b3f1191e98085aeecef4e31ff8535be78
Корсаев Артемий Батаевич <abkorsaev@edu.hse.ru>
2025-07-13 09:39:23 +0300

    feat(data_app): add inference showcase

--- data_app/streamlit_app.py
+++ data_app/streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 
+import os
+
+print(os.getcwd())
+
 st.title("An inference showcase of my reimplementation")
 
-video_file = open("inference showcase.mp4", "rb")
+video_file = open("./data_app/inference showcase.mp4", "rb")
 video_bytes = video_file.read()
+
+st.video(video_bytes)
